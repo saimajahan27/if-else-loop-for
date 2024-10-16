d1b4c68ad552485414cf6f174d6f74027d7dde1e
saimajahan27 <ls9894894@gmail.com>
2024-10-16 14:35:01 +0600

    first commit

---
+++ length.js
@@ -0,0 +1,40 @@
+// cheak length
+const numbers =[1, 2, 4, 27, 35, 50, 60]
+console.log(numbers.length);
+// cheak length
+// cheack index
+// console.log(numbers[4]);
+// cheack index
+// cheack indexof
+// console.log(numbers.indexOf(50));
+// cheack indexof
+// cheack push
+// numbers.push(76);
+// console.log(numbers);
+// cheack push
+// cheack pop
+// numbers.pop("70");
+// console.log(numbers);
+// cheack pop
+// cheack shift
+const team = ["Saima","Labuny","Saeva","Muslima","Safa"]
+// team.shift()
+// console.log(team);
+// cheack shift
+// cheack unshift
+// team.unshift("mim")
+// console.log(team);
+// cheack unshift
+// cheack push
+// team.push("Jannat");
+// console.log(team);
+// cheack push
+// cheak pop
+// team.pop("Safa");
+// console.log(team);
+// cheak pop
+// cheack includes
+console.log(team.includes("Saima"));
+console.log(team.includes("Labuny"));
+console.log(Array.isArray(team));
+// cheack includes
